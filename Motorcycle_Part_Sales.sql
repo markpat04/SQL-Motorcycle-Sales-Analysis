875c7ccf503254b30b8c23b25532e4ef16292ffd
Pattarapol Vanitchayakitpaiboon <163713528+markpat04@users.noreply.github.com>
2025-10-25 12:26:39 +0700

    Add SQL query for wholesale net revenue by month

---
+++ Motorcycle_Part_Sales.sql
@@ -0,0 +1,12 @@
+SELECT
+	product_line,
+	CASE WHEN EXTRACT('month' from date) = 6 THEN 'June'
+		WHEN EXTRACT('month' from date) = 7 THEN 'July'
+		WHEN EXTRACT('month' from date) = 8 THEN 'August'
+		END AS month,
+	warehouse,
+	SUM(total) - SUM(payment_fee) AS net_revenue
+FROM Sales
+WHERE client_type = 'Wholesale'
+GROUP BY product_line, warehouse, month
+ORDER BY product_line, month, net_revenue DESC
